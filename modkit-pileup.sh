afc4c9827b6f05da536e7d3c2ad7e60d72cac757
Oran Gutiérrez Fugón <ojgutierrez@ucdavis.edu>
2023-07-06 13:39:31 -0700

    modkit

--- modkit-pileup.sh
+++ modkit-pileup.sh
@@ -1,30 +1,10 @@
 #!/bin/bash
-# Load samtools module
-module load samtools
 
-# Find BAM file with "concat" in the name in the current directory
-bam_file=$(find . -name '*concat*.bam' -type f -print -quit)
+module load modkit
 
-# Check if a BAM file is found
-if [ -z "$bam_file" ]; then
-  echo "No BAM file with 'concat' in the name found."
-  exit 1
-fi
+modkit pileup --bedgraph --ref /share/lasallelab/Oran/dovetail/luhmes/methylation/concats/hg38refGenome --log-filepath /share/lasallelab/Oran/dovetail/luhmes/methylation/concatenated/NP4-3/NP4-3pileup.log --threads 20 --num-reads 100000 /share/lasallelab/Oran/dovetail/luhmes/methylation/concatenated/NP4-3/NP4-3concat_sorted.bam /share/lasallelab/Oran/dovetail/luhmes/methylation/concatenated/NP4-3 
 
-# Get the base filename without the extension
-base_name=$(basename "$bam_file" .bam)
-
-# Remove "concat" from the base filename
-new_base_name="${base_name/concat/}"
-
-# Perform samtools sort on the BAM file and save to a new sorted BAM file
-sorted_file="${new_base_name}_sorted.bam"
-samtools sort -o "$sorted_file" "$bam_file"
-
-# Perform samtools index on the sorted file
-samtools index "$sorted_file"
-
-# Print the completed message after the code finishes running (It's huge so I can see the screen change from far away 8).  Bonus points if you get the reference!
+# Prints this scary message after the ghost in the shell finishes running
 echo "
                                                      @@@@@@@@
                                              %&&&&@&          @@@@@(@&@&&#                                                             
@@ -83,11 +63,11 @@ echo "
                            %%%%&&&&&&&&@&@*                      &@                     &&%                                            
                                                                    (@&&(&&&&&&&&&&&&(%@&&*  
 
-TTTTTT H  H IIIII  SSS     H   H  OOO   U   U   SSS   EEEEE   IIIII  SSS      CCC  L    EEEE     A    N     N   !!
-  TT   H  H   I   S    S   H   H O   O  U   U  S   S  E         I   S   S    C     L    E       A A   NN    N   !!
-  TT   H  H   I    SS      H   H O   O  U   U   S     E         I    S       C     L    E      A   A  N N   N   !!
-  TT   HHHH   I      S     HHHHH O   O  U   U    S    EEE       I      S     C     L    EEE   AAAAAAA N  N  N   !!
-  TT   H  H   I       S    H   H O   O  U   U      S  E         I       S    C     L    E     A     A N   N N   !!
-  TT   H  H   I   S   S    H   H O   O  U   U  S   S  E         I   S   S    C     L    E     A     A N    NN   
-  TT   H  H IIIII  SSS     H   H  OOO    UUU    SSS   EEEEE   IIIII  SSS      CCC  LLLL EEEE  A     A N     N   !!
+TTTTTT H  H IIIII  SSS     H   H  OOO   U   U   SSS   EEEEE   IIIII  SSS      CCC  L    EEEE     A    N     N
+  TT   H  H   I   S    S   H   H O   O  U   U  S   S  E         I   S   S    C     L    E       A A   NN    N
+  TT   H  H   I    SS      H   H O   O  U   U   S     E         I    S       C     L    E      A   A  N N   N
+  TT   HHHH   I      S     HHHHH O   O  U   U    S    EEE       I      S     C     L    EEE   AAAAAAA N  N  N
+  TT   H  H   I       S    H   H O   O  U   U      S  E         I       S    C     L    E     A     A N   N N
+  TT   H  H   I   S   S    H   H O   O  U   U  S   S  E         I   S   S    C     L    E     A     A N    NN
+  TT   H  H IIIII  SSS     H   H  OOO    UUU    SSS   EEEEE   IIIII  SSS      CCC  LLLL EEEE  A     A N     N
 "
