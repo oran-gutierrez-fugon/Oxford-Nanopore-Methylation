da47164dc3867d5ea963c4e1ecbf1eb0ce533668
Oran Gutiérrez Fugón <ojgutierrez@ucdavis.edu>
2023-08-22 04:44:24 -0700

    more work on shell script

--- Shell Scripts/Prenanomethphase-NewBam-NP4-3.sh
+++ Shell Scripts/Prenanomethphase-NewBam-NP4-3.sh
@@ -3,15 +3,21 @@
 #concatenates sequencing summary file to enable and speed up indexing
 cat /share/lasallelab/Oran/dovetail/luhmes/nanoporeRAW/PROM0151_LaSalle_NP4_3_05182023/20230518_1823_3E_PAO28520_86921a60/fast5/basecalling/sequencing_summary.txt /share/lasallelab/Oran/dovetail/luhmes/nanoporeRAW/PROM0151_LaSalle_NP4_3_05192023_PF/20230519_1725_3E_PAO28520_cbd61198/fast5/basecalling/sequencing_summary.txt /share/lasallelab/Oran/dovetail/luhmes/nanoporeRAW/PROM0151_LaSalle_NP4_3_05202023_PF2/20230520_1804_3E_PAO28520_6c925a53/fast5/basecalling/sequencing_summary.txt > /share/lasallelab/Oran/dovetail/luhmes/nanoporeRAW/NP4-3-concatpass/seqsummarycat/sequencing_summary.txt
 
+#activates nanopolish env and loads samtools
 conda activate nanopolish
 module load samtools
 
 #index fastq files with fast5 for concat NP4-3
 nanopolish index -d /share/lasallelab/Oran/dovetail/luhmes/nanoporeRAW/fast5/NP4-3/ -s /share/lasallelab/Oran/dovetail/luhmes/nanoporeRAW/NP4-3-concatpass/seqsummarycat/sequencing_summary.txt /share/lasallelab/Oran/dovetail/luhmes/nanoporeRAW/NP4-3-concatpass/NP4-3-cat.fastq.gz
 
-#methylation calling (failed since no index db file) (may need to wait for new install)
+#sorts and indexes minimap created bam file from fastqconcats directory (already done for NP4-3)
+samtools sort -o /share/lasallelab/Oran/dovetail/luhmes/methylation/fastqconcats/NP4-3-cat-sorted.bam /share/lasallelab/Oran/dovetail/luhmes/methylation/fastqconcats/NP4-3-cat.bam
+samtools index /share/lasallelab/Oran/dovetail/luhmes/methylation/fastqconcats/NP4-3-cat-sorted.bam
+
+#methylation calling (failed since no index db file should work now that index may be working for concat, but should wait for new install)
 nanopolish call-methylation -t 40 -q cpg -r /share/lasallelab/Oran/dovetail/luhmes/nanoporeRAW/NP4-3-concatpass/NP4-3-cat.fastq.gz -b /share/lasallelab/Oran/dovetail/luhmes/methylation/fastqconcats/NP4-3-cat-sorted.bam -g /share/lasallelab/Oran/dovetail/refgenomes/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna > /share/lasallelab/Oran/dovetail/luhmes/methylation/fastqconcats/NP4-3-MethylationCall.tsv
 
+#activates clair3 env and loads samtools
 conda deactivate
 conda activate clair3-1.0.4
 module load samtools
@@ -22,14 +28,13 @@ run_clair3.sh --bam_fn=/share/lasallelab/Oran/dovetail/luhmes/methylation/fastqc
 #filters for passed quality
 gunzip -c /share/lasallelab/Oran/dovetail/luhmes/methylation/concatenated/NP4-3/clair3/merge_output.vcf.gz | awk '$1 ~ /^#/ || $7=="PASS"' > /share/lasallelab/Oran/dovetail/luhmes/methylation/concatenated/NP4-3/clair3/PassedVariants.vcf
 
-#change and activate whatshap environment
+#activates whatshap env and loads samtools
 conda deactivate
 conda activate /share/lasallelab/Oran/miniconda3/whatshap-env
 module load samtools
 
-#phasing
+#phasing with whatshap
 whatshap phase --ignore-read-groups --reference /share/lasallelab/Oran/dovetail/refgenomes/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna -o /share/lasallelab/Oran/dovetail/luhmes/methylation/concatenated/NP4-3/clair3/NP4-3-whatshap_phased.vcf /share/lasallelab/Oran/dovetail/luhmes/methylation/concatenated/NP4-3/clair3/PassedVariants.vcf /share/lasallelab/Oran/dovetail/luhmes/methylation/fastqconcats/NP4-3-cat-sorted.bam
-
 
 
 # Prints this scary message after the ghost in the shell finishes running.  RIP: Zelda Rubinstein & Heather O'Rourke
