0bd267fbb11f8dec012cac61ce94b41d391c02a7
Oran Gutiérrez Fugón <ojgutierrez@ucdavis.edu>
2023-10-16 18:02:50 -0700

    yup still refining this

--- Shell Scripts/nanometh_UDP4-2-PFr10.sh
+++ Shell Scripts/nanometh_UDP4-2-PFr10.sh
@@ -5,30 +5,31 @@ module load samtools
 module load minimap2/2.24
 
 # changes folder & cat fastq (specific folders vary)
-cd /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/PROM0151_LaSalle_UDP4-2_05042023_PF/20230504_1405_2G_PAM05619_b1291f1b/fast5/basecalling/pass/
-cat *.fastq.gz > /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/UDP4-2-PF.fastq.gz
+cd /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/PROM0151_LaSalle_UDP4-2_05032023/20230503_1715_2G_PAM05619_2a82e598/fast5/basecalling/pass/
+cat *.fastq.gz > /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/UDP4-2-.fastq.gz
 
 # create slow5 (or blow5) from single read fast5 (slow5tools was installed in my base miniconda env)
 conda activate
-slow5tools fast5toslow5 -p 60 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/PROM0151_LaSalle_UDP4-2_05042023_PF/20230504_1405_2G_PAM05619_b1291f1b/fast5 -d /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2/PF
+mkdir /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2/-/
+slow5tools fast5toslow5 -p 60 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/PROM0151_LaSalle_UDP4-2_05032023/20230503_1715_2G_PAM05619_2a82e598/fast5 -d /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2/-
 
 # must merge multiple slow5 (or binary blow5)
-mkdir /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2/PF/blow/
-slow5tools merge -t 60 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2/PF -o /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2/PF/blow/UDP4-PF.blow5
+mkdir /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2/-/blow/
+slow5tools merge -t 60 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2/- -o /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2/-/blow/UDP4-.blow5
 
 # remove multiple slow5 (or blow5)
-rm /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2/PF/*.slow5
+rm /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2/-/*.slow5
 
 # index using optimized fork of nanopolish f5c that supports R10 chemistry
 conda activate f5c
-f5c index -t 60 --slow5 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2/PF/blow/UDP4-PF.blow5 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/UDP4-2-PF.fastq.gz 
+f5c index -t 60 --iop 60 -d /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/PROM0151_LaSalle_UDP4-2_05042023_PF/20230504_1405_2G_PAM05619_b1291f1b/fast5/ /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/UDP4-2-PF.fastq.gz 
 
 # maps to ref creates sorts and indexes bam
-minimap2 -a -x map-ont /share/lasallelab/Oran/dovetail/refgenomes/hg19.fa.gz /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/UDP4-2-PF.fastq.gz | samtools sort -T tmp -o /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2_sorted.bam
-samtools index /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2_sorted.bam; echo "UDP4-2-PF f5c index done" | mail -s "UDP4-2-PF f5c index done" ojg333@gmail.com
+minimap2 -a -x map-ont -t 60 -2 /share/lasallelab/Oran/dovetail/refgenomes/hg19.fa.gz /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/UDP4-2-PF.fastq.gz | samtools sort -T tmp -o /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2-PF_sorted.bam
+samtools index /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2-PF_sorted.bam; echo "It is done" | mail -s "UDP4-2-PF samtools index" ojg333@gmail.com
 
 #f5c more efficient caller, use slow5 directories and concatenated fastq used in minimap step
-f5c call-methylation -t 50 --pore r10 -x hpc-high --meth-out-version 2 --slow5 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2/PF2/UDP4-PF2.blow5 -r /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/UDP4-2-PF2.fastq.gz  -b /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2-PF2_sorted.bam -g /share/lasallelab/Oran/dovetail/refgenomes/hg19.fa > /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2-PF2-MethylationCall.tsv; echo "UDP4-2 f5c call-methylation done" | mail -s "UDP4-2 f5c call-methylation done" ojg333@gmail.com
+f5c call-methylation --pore r10 -x hpc-high --meth-out-version 2 --skip-unreadable=no  -r /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/UDP4-2-PF.fastq.gz  -b /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2-PF_sorted.bam -g /share/lasallelab/Oran/dovetail/refgenomes/hg19.fa > /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2-PF-MethylationCall2.tsv; echo "It is done" | mail -s "UDP4-2-PF f5c methylation" ojg333@gmail.com
 
 # nanomethphase conda env and python step 1 methyl call processor
 conda deactivate
--- Shell Scripts/nanomethphase-NP4-3r10indel.sh
+++ Shell Scripts/nanomethphase-NP4-3r10indel.sh
@@ -11,83 +11,82 @@ module load minimap2/2.24
 #concatenates all basecalled fastqs from all flushes to catcat folder (do not use methylation fastqs) see nanomethphase github
 #May want to remove individual flush concat fastq at the end but recommend leave them until you finish the pipeline in case you need to process flushes individualy and then merge the methylation call tsv
 #*****WARNING***** input fastq preflush,PF,PF2 folders must be verified with cd one by one not just find/replace replicate name
-cat /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/PROM0151_LaSalle_NP4_4_05182023/20230518_1823_3G_PAO36704_d713bcfc/fast5/basecalling/pass/*.fastq.gz > /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/UDP4-2-.fastq.gz 
-cat /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/PROM0151_LaSalle_NP4_4_05192023_PF/20230519_1725_3G_PAO36704_22df49af/fast5/basecalling/pass/*.fastq.gz > /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/UDP4-2-PF.fastq.gz 
-cat /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/PROM0151_LaSalle_NP4_4_05202023_PF2/20230520_1804_3G_PAO36704_e7bf9f77/fast5/basecalling/pass/*.fastq.gz > /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/UDP4-2-PF2.fastq.gz
+cat /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/PROM0151_LaSalle_NP4_4_05182023/20230518_1823_3G_PAO36704_d713bcfc/fast5/basecalling/pass/*.fastq.gz > /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/NP4-4-.fastq.gz 
+cat /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/PROM0151_LaSalle_NP4_4_05192023_PF/20230519_1725_3G_PAO36704_22df49af/fast5/basecalling/pass/*.fastq.gz > /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/NP4-4-PF.fastq.gz 
+cat /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/PROM0151_LaSalle_NP4_4_05202023_PF2/20230520_1804_3G_PAO36704_e7bf9f77/fast5/basecalling/pass/*.fastq.gz > /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/NP4-4-PF2.fastq.gz
 
-cat /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/UDP4-2-.fastq.gz /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/UDP4-2-PF.fastq.gz /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/UDP4-2-PF2.fastq.gz > /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/UDP4-2.fastq.gz
+cat /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/NP4-4-.fastq.gz /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/NP4-4-PF.fastq.gz /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/NP4-4-PF2.fastq.gz > /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/NP4-4.fastq.gz
 
 #converts fast5 raw files to more efficient blow5 and puts all flushes in same directory. slow5tools was installed in base conda env
 #*****WARNING***** input fast5 preflush,PF,PF2 folders must be verified with cd one by one not just find/replace replicate name
 conda activate
-slow5tools fast5toslow5 -p 60 --to blow5 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/PROM0151_LaSalle_NP4_4_05182023/20230518_1823_3G_PAO36704_d713bcfc/fast5 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/PROM0151_LaSalle_NP4_4_05192023_PF/20230519_1725_3G_PAO36704_22df49af/fast5 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/PROM0151_LaSalle_NP4_4_05202023_PF2/20230520_1804_3G_PAO36704_e7bf9f77/fast5 -d /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2; echo "It is done" | mail -s "UDP4-2 fast5toslow5" ojg333@gmail.com
+slow5tools fast5toslow5 -p 60 --to blow5 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/PROM0151_LaSalle_NP4_4_05182023/20230518_1823_3G_PAO36704_d713bcfc/fast5 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/PROM0151_LaSalle_NP4_4_05192023_PF/20230519_1725_3G_PAO36704_22df49af/fast5 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/PROM0151_LaSalle_NP4_4_05202023_PF2/20230520_1804_3G_PAO36704_e7bf9f77/fast5 -d /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/NP4-4; echo "It is done" | mail -s "NP4-4 fast5toslow5" ojg333@gmail.com
 
 #merges slow5 dir to 1 single blow5 file
-mkdir /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2/blow/
-slow5tools merge -t 60 --to blow5 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2  -o /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2/blow/UDP4-2cat.blow5
+mkdir /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/NP4-4/blow/
+slow5tools merge -t 60 --to blow5 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/NP4-4  -o /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/NP4-4/blow/NP4-4cat.blow5
 
 #Cleans up single blow5 files since they are no longer needed
-rm /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2/*.slow5 
+rm /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/NP4-4/*.slow5 
 
 # activates f5c env and indexes blow5 and fastq
 conda activate f5c
-f5c index -t 60 --slow5 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2/blow/UDP4-2cat.blow5 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/UDP4-2.fastq.gz
+f5c index -t 60 --slow5 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/NP4-4/blow/NP4-4cat.blow5 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/NP4-4.fastq.gz
 
 #minimap2 aligment + samtools sam to bam + indexes bam
-minimap2 -a -x map-ont -t 60 -2 /share/lasallelab/Oran/dovetail/refgenomes/hg19.fa.gz /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/UDP4-2.fastq.gz | samtools sort -T tmp -o /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2_sorted.bam
-samtools index /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2_sorted.bam; echo "UDP4-2 samtools index done title" | mail -s "UDP4-2 samtools index done content" ojg333@gmail.com
+minimap2 -a -x map-ont -t 60 -2 /share/lasallelab/Oran/dovetail/refgenomes/hg19.fa.gz /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/NP4-4.fastq.gz | samtools sort -T tmp -o /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4_sorted.bam
+samtools index /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4_sorted.bam; echo "NP4-4 samtools index done title" | mail -s "NP4-4 samtools index done content" ojg333@gmail.com
 
 #cleans up any previous methylation runs if needed
-#rm /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2-MethylationCall.tsv
+#rm /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4-MethylationCall.tsv
 
 #methylation calling with f5c (more efficient program than nanopolish) must include option --pore r10 for r10 chemistry (thanks Logan!)
-f5c call-methylation --pore r10 -x hpc-high --meth-out-version 2 --slow5 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/UDP4-2/blow/UDP4-2cat.blow5 -r /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/UDP4-2.fastq.gz  -b /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2_sorted.bam -g /share/lasallelab/Oran/dovetail/refgenomes/hg19.fa > /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2-MethylationCall.tsv; echo "UDP4-2 f5c call-methylation done" | mail -s "UDP4-2 f5c call-methylation done" ojg333@gmail.com
+f5c call-methylation --pore r10 -x hpc-high --meth-out-version 2 --slow5 /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/slow5/NP4-4/blow/NP4-4cat.blow5 -r /share/lasallelab/Oran/dovetail/luhmes/nanoRAW/catcat/NP4-4.fastq.gz  -b /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4_sorted.bam -g /share/lasallelab/Oran/dovetail/refgenomes/hg19.fa > /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4-MethylationCall.tsv; echo "NP4-4 f5c call-methylation done" | mail -s "NP4-4 f5c call-methylation done" ojg333@gmail.com
 
 #cleans up any previous passed variants vcf to avoid mixed data errors
-#cd /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2/clair3/
-#rm /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2/clair3/*.*
-#rm -r /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2/clair3/tmp
-#rm -r /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2/clair3/log
+#cd /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4/clair3/
+#rm /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4/clair3/*.*
+#rm -r /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4/clair3/tmp
+#rm -r /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4/clair3/log
 
 #activates clair3 env and loads samtools
 conda activate clair3-1.0.4
 module load samtools
 
-#UDP4-2 variant calling fastqconcats new bam from minimap using hg19 ref. Model is dependent on chemistry and basecaller used (see clair3 and reiro githubs)
-run_clair3.sh --bam_fn=/share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2_sorted.bam --ref_fn=/share/lasallelab/Oran/dovetail/refgenomes/hg19.fa --output=/share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2/clair3 --threads=60 --platform=ont --model_path=/share/lasallelab/Oran/dovetail/luhmes/methylation/clair3model/rerio/clair3_models/r1041_e82_400bps_sup_g615
+#NP4-4 variant calling fastqconcats new bam from minimap using hg19 ref. Model is dependent on chemistry and basecaller used (see clair3 and reiro githubs)
+run_clair3.sh --bam_fn=/share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4_sorted.bam --ref_fn=/share/lasallelab/Oran/dovetail/refgenomes/hg19.fa --output=/share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4/clair3 --threads=60 --platform=ont --model_path=/share/lasallelab/Oran/dovetail/luhmes/methylation/clair3model/rerio/clair3_models/r1041_e82_400bps_sup_g615
 
 #filters for passed quality leave unzipped for now since will need to be in bgzip compression format for downstream steps
-gunzip -c /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2/clair3/merge_output.vcf.gz | awk '$1 ~ /^#/ || $7=="PASS"' > /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2/clair3/UDP4-2-PassedVariants.vcf
+gunzip -c /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4/clair3/merge_output.vcf.gz | awk '$1 ~ /^#/ || $7=="PASS"' > /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4/clair3/NP4-4-PassedVariants.vcf
 
 #indexes vcf file in prep for whatshap with bgzip then indexes with tabix. Using HiChIP oj conda env with bgzip and tabix already installed 
 conda deactivate
 conda activate /share/lasallelab/Oran/dovetail/luhmes/merged/oj
-bgzip -i /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2/clair3/UDP4-2-PassedVariants.vcf
-tabix -p vcf /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2/clair3/UDP4-2-PassedVariants.vcf.gz
+bgzip -i /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4/clair3/NP4-4-PassedVariants.vcf
+tabix -p vcf /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4/clair3/NP4-4-PassedVariants.vcf.gz
 
 #switches to whatshap conda env
 conda deactivate
 conda activate /share/lasallelab/Oran/miniconda3/whatshap-env
 
-#phasing with whatshap (reference must be uncompressed and indexed).  Since whatshap is not optimized for multicore this is a good step to do in parallel with other samples on the cluster after running clair3 separately or together with clair3 running for the next sample. May also substitute illumina variant vcf file if available. --indels option not needed in whatshap version > 2.0
-whatshap phase --ignore-read-groups --reference /share/lasallelab/Oran/dovetail/refgenomes/hg19.fa -o /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2/clair3/UDP4-2-whatshap_phased.vcf /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2/clair3/UDP4-2-PassedVariants.vcf.gz /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2_sorted.bam
+#phasing with whatshap (reference must be uncompressed and indexed).  Since whatshap is not optimized for multicore this is a good step to do in parallel with other samples on the cluster after running clair3 separately or together with clair3 running for the next sample. May also substitute illumina variant vcf file if available
+whatshap phase --ignore-read-groups --indels --reference /share/lasallelab/Oran/dovetail/refgenomes/hg19.fa -o /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4/clair3/NP4-4-indel_phased.vcf /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4/clair3/NP4-4-PassedVariants.vcf.gz /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4_sorted.bam
 
-#activates nanomethphase conda env and python step 1 methyl call processor + index (Thanks Osman!)
+#activates nanomethphase conda env and python step 1 methyl call processor (Thanks Osman!)
 conda deactivate
 conda activate /share/lasallelab/Oran/test_nanomethphase/NanoMethPhase/nanometh-environment
 cd /share/lasallelab/Oran/test_nanomethphase/NanoMethPhase
-python nanomethphase.py methyl_call_processor -mc /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2-MethylationCall.tsv -t 60 | sort -k1,1 -k2,2n -k3,3n | bgzip > /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2-MethylationCall.bed.gz && tabix -p bed /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2-MethylationCall.bed.gz
+python nanomethphase.py methyl_call_processor -mc /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4-MethylationCall.tsv -t 60 | sort -k1,1 -k2,2n -k3,3n | bgzip > /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4-MethylationCall.bed.gz && tabix -p bed /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4-MethylationCall.bed.gz
 
 #phases the methylome (the moment you've all been waiting for)
-python nanomethphase.py phase --include_indels -b /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2_sorted.bam -v /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2/clair3/UDP4-2-whatshap_phased.vcf -mc /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2-MethylationCall.bed.gz -r /share/lasallelab/Oran/dovetail/refgenomes/hg19.fa -o /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2_methylome -of bam,methylcall,bam2bis -t 60
+python nanomethphase.py phase --include_indels -b /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4_sorted.bam -v /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4/clair3/NP4-4-indel_phased.vcf -mc /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4-MethylationCall.bed.gz -r /share/lasallelab/Oran/dovetail/refgenomes/hg19.fa -o /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4_methylome -of bam,methylcall,bam2bis -t 60
 
 #aggregates data from both strands (requieres datamash installation)
 #use correct file names from previous step
-sed '1d' /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2_methylome_NanoMethPhase_HP1_MethylFrequency.tsv | awk -F'\t' '{if ($4=="-") {$2=$2-1;$3=$3-1}; print $1,$2,$3,$5,$6}' OFS='\t' | sort -k1,1 -k2,2n | datamash -g1,2,3 sum 4,5 | awk -F'\t' '{print $0,$5/$4}' OFS='\t' | sed '1i chromosome\tstart\tend\tNumOfAllCalls\tNumOfModCalls\tMethylFreq' > /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2/UDP4-2_aggregated_HP1_MethylFrequency.tsv
+sed '1d' NP4-4_methylome_HP1_MethylFrequency.tsv | awk -F'\t' '{if ($4=="-") {$2=$2-1;$3=$3-1}; print $1,$2,$3,$5,$6}' OFS='\t' | sort -k1,1 -k2,2n | datamash -g1,2,3 sum 4,5 | awk -F'\t' '{print $0,$5/$4}' OFS='\t' | sed '1i chromosome\tstart\tend\tNumOfAllCalls\tNumOfModCalls\tMethylFreq' > /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/NP4-4/HP1_MethylFrequency.tsv
 
-#Differential methylation analysis (if you've made it this far, lets go a little farther)
-#Check folders and file names match with previous steps
-python nanomethphase.py dma -c 1,2,4,5,7 -ca /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2/UDP4-2_aggregated_HP1_MethylFrequency.tsv -co /share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2/UDP4-2_aggregated_HP2_MethylFrequency.tsv -o </share/lasallelab/Oran/dovetail/luhmes/methylation/phasing/UDP4-2/ -op DMA
+
+
 
 # Prints this scary message after the ghost in the shell finishes running.  Bonus points if you get the reference, RIP: Zelda Rubinstein & Heather O'Rourke
 echo "
